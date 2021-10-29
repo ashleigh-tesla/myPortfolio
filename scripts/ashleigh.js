15a1edf97ada1e76c75035f7008d33bdc66c121a
ashleigh-tesla <84648968+ashleigh-tesla@users.noreply.github.com>
2021-10-29 10:47:06 +0200

    updated index.html ; ashleigh.css & ashleigh.js

--- scripts/ashleigh.js
+++ scripts/ashleigh.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
     $(window).scroll(function() {
-            if (this.scrollY > 20) {
-                $('.navbar').addClass('sticky');
-            } else {
-                $('.navbar').removeClass('sticky');
-            }
-            if (this.scroll > 500) {
-                $('.scroll-up-btn').addClass("show")
-            } else {
-                $('.scroll-up-btn').removeClass("show")
-            }
-        })
-        // slide-up script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass('sticky');
+        } else {
+            $('.navbar').removeClass('sticky');
+        }
+        if (this.scroll > 500) {
+            $('.scroll-up-btn').addClass("show")
+        } else {
+            $('.scroll-up-btn').removeClass("show")
+        }
+    })
+
+    // slide-up script
     $('.scroll-up-btn').click(function() {
         $('html').animate({ scrollTop: 0 })
     })
@@ -23,8 +24,14 @@ $(document).ready(function() {
     })
 
     // typing animation script
-    var typed = new Typed(".typing", {
+    var typed = new Typed(".typing-1", {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "UI/UX Designer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
